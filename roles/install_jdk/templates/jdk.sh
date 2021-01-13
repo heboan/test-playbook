0f5a073fba65cf8bd2bc8b87224ca52744ab34bf
heboan <heboan@qq.com>
2021-01-14 01:24:58 +0800

    新增supervisord和磁盘分区

--- roles/install_jdk/templates/jdk.sh
+++ roles/install_jdk/templates/jdk.sh
@@ -1,5 +1,5 @@
-export JAVA_HOME=/usr/java/jdk
-export JAVA_BIN=/usr/java/jdk/bin
+export JAVA_HOME=/usr/java/default
+export JAVA_BIN=/usr/java/default/bin
 export PATH=$PATH:$JAVA_HOME/bin
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 export JAVA_HOME JAVA_BIN PATH CLASSPATH
